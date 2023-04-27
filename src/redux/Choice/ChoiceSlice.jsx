b6a21920d0f9b319edde05a95592238f5aa0615b
benanakkaya <ahmetbenanakkaya@gmail.com>
2023-04-27 15:25:28 +0300

    fix

--- src/redux/Choice/ChoiceSlice.jsx
+++ src/redux/Choice/ChoiceSlice.jsx
@@ -1,6 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-import { set } from "mongoose";
 
 export const addNewChoice = createAsyncThunk(
   "choice/new-choice",
